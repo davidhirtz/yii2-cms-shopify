d556cf2c8ad3b1782de4ac8ac43ed7254bc6510f
davidhirtz <david.hirtz@me.com>
2022-09-01 16:33:36 +0200

    Update ProductIdColumn.php

--- widgets/grid/columns/ProductIdColumn.php
+++ widgets/grid/columns/ProductIdColumn.php
@@ -5,20 +5,38 @@ namespace davidhirtz\yii2\cms\shopify\widgets\grid\columns;
 use davidhirtz\yii2\cms\modules\admin\widgets\grid\EntryGridView;
 use davidhirtz\yii2\shopify\models\Product;
 use davidhirtz\yii2\skeleton\helpers\Html;
+use Yii;
 use yii\db\ActiveRecord;
-use yii\grid\Column;
+use yii\grid\DataColumn;
 
 /**
  * ProductIdColumn can be implemented by {@see EntryGridView} to add a product column.
  *
  * @property EntryGridView $grid
  */
-class ProductIdColumn extends Column
+class ProductIdColumn extends DataColumn
 {
+    /**
+     * @var string
+     */
+    public $attribute = 'product_id';
+
     /**
      * @var Product[]
      */
     private static $_products;
+
+    /**
+     * @return void
+     */
+    public function init()
+    {
+        // Only show products if any are loaded on the page
+        $this->visible = $this->visible && count($this->getProducts()) > 0;
+        $this->label ??= Yii::t('shopify', 'Product');
+
+        parent::init();
+    }
 
     /**
      * @param ActiveRecord $model
@@ -28,7 +46,7 @@ class ProductIdColumn extends Column
      */
     protected function renderDataCellContent($model, $key, $index)
     {
-        if ($product = ($this->getProducts()[$model->$key] ?? null)) {
+        if ($product = ($this->getProducts()[$model->getAttribute($this->attribute)] ?? null)) {
             $name = $product->status == $model->getAttribute('status') ? $product->name : Html::iconText($product->getStatusIcon(), $product->name, [
                 'title' => $product->getStatusName(),
                 'data-toggle' => 'tooltip',
@@ -50,7 +68,7 @@ class ProductIdColumn extends Column
             $productIds = [];
 
             foreach ($this->grid->dataProvider->getModels() as $model) {
-                if ($productId = $model->getAttribute('id')) {
+                if ($productId = $model->getAttribute($this->attribute)) {
                     $productIds[] = $productId;
                 }
             }
@@ -58,7 +76,8 @@ class ProductIdColumn extends Column
             if ($productIds) {
                 static::$_products = Product::find()
                     ->select(['id', 'status', 'name'])
-                    ->indexBy('name')
+                    ->andWhere(['id' => $productIds])
+                    ->indexBy('id')
                     ->all();
             }
         }
