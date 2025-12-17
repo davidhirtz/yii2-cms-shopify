2f1324a4d8fe7a8ed0a8b36e1c6bcdcabcb3f6e1
davidhirtz <david.hirtz@me.com>
2025-12-17 13:36:46 +0000

    Fixed import paths

--- src/Widgets/Grids/Columns/ProductIdColumn.php
+++ src/Widgets/Grids/Columns/ProductIdColumn.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Hirtz\Cms\shopify\widgets\grids\columns;
 
 use Hirtz\Cms\Models\ActiveRecord;
-use Hirtz\Cms\Modules\Admin\Widgets\Forms\EntryGridView;
+use Hirtz\Cms\Modules\Admin\Widgets\Grids\EntryGridView;
 use Hirtz\Shopify\Models\Product;
 use Hirtz\Skeleton\Helpers\Html;
 use Yii;
