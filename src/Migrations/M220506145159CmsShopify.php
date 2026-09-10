93261ff21b4ffb17bb10ba135f5f1f1046070331
davidhirtz <david.hirtz@me.com>
2026-09-10 14:16:07 +0200

    Remove enableI18nTables support
    Inline the i18nTablesCallback wrapper in the migration.

--- src/Migrations/M220506145159CmsShopify.php
+++ src/Migrations/M220506145159CmsShopify.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Hirtz\Cms\Shopify\Migrations;
 
-use Hirtz\Cms\Migrations\Traits\I18nTablesTrait;
 use Hirtz\Cms\Models\Entry;
 use Hirtz\Shopify\Models\Product;
 use Hirtz\Skeleton\Db\Traits\MigrationTrait;
@@ -17,33 +16,28 @@ use yii\db\Migration;
 class M220506145159CmsShopify extends Migration
 {
     use MigrationTrait;
-    use I18nTablesTrait;
 
     public function safeUp(): void
     {
-        $this->i18nTablesCallback(function (): void {
-            $this->addColumn(Entry::tableName(), 'product_id', (string)$this->bigInteger()
-                ->unsigned()
-                ->null()
-                ->after('type'));
+        $this->addColumn(Entry::tableName(), 'product_id', (string)$this->bigInteger()
+            ->unsigned()
+            ->null()
+            ->after('type'));
 
-            $this->createIndex('product_id', Entry::tableName(), 'product_id', true);
+        $this->createIndex('product_id', Entry::tableName(), 'product_id', true);
 
-            $this->addForeignKey(
-                $this->getForeignKeyName(Entry::tableName(), 'product_id_ibfk'),
-                Entry::tableName(),
-                'product_id',
-                Product::tableName(),
-                'id',
-                'SET NULL'
-            );
-        });
+        $this->addForeignKey(
+            $this->getForeignKeyName(Entry::tableName(), 'product_id_ibfk'),
+            Entry::tableName(),
+            'product_id',
+            Product::tableName(),
+            'id',
+            'SET NULL'
+        );
     }
 
     public function safeDown(): void
     {
-        $this->i18nTablesCallback(function (): void {
-            $this->dropColumn(Entry::tableName(), 'product_id');
-        });
+        $this->dropColumn(Entry::tableName(), 'product_id');
     }
 }
